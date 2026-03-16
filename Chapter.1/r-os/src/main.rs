55551473f04422edaa7ebd1bf38bd3cad169c2fa
qingzhixing <qzsqqmail@qq.com>
2026-03-16 17:44:44 +0800

    📦 NEW: 使用linker.ld中的信息来实现main函数中clear_bss函数

--- Chapter.1/r-os/src/main.rs
+++ Chapter.1/r-os/src/main.rs
@@ -7,5 +7,23 @@ global_asm!(include_str!("entry.asm"));
 
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
+    clear_bss();
     loop {}
+}
+
+fn clear_bss() {
+    unsafe extern "C" {
+        static sbss: u8;
+        static ebss: u8;
+    }
+
+    let start_bss = ((unsafe { &sbss }) as *const u8) as usize;
+    let end_bss = ((unsafe { &ebss }) as *const u8) as usize;
+
+    for addr in start_bss..end_bss {
+        unsafe {
+            // 使用 write_volatile 防止编译器优化
+            (addr as *mut u8).write_volatile(0);
+        }
+    }
 }
